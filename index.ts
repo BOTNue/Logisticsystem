a182745cdae23c149f94d12c2fd6e29d8d460299
Joshua Fourie <joshua@fourie.se>
2024-11-22 20:19:39 +0100

    Implementation: Implemented a get route for pickers and made a put route for updating order setting the status as completed. There is a get route for orders to se pending orders and completed orders.

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import { Picker } from "./models/picker"
 import { Order } from "./models/order"
 
 
-const db = await mongoose.connect("mongodb+srv://joshua:BEjnCT1vVMK8Wg8S@cluster0.s9dkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+const db = await mongoose.connect("mongodb+srv://<username>:<db_password>@cluster0.s9dkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 interface OrderRequestBody {
     order_id: string;
@@ -17,8 +17,18 @@ interface OrderRequestBody {
     picker_id: string;
 }
 
+interface OrderUpdateBody {
+    picker_id: string;
+    status: string;
+    products: Array<{
+        product_id: string;
+        quantity: number;
+    }>;
+}
+
 new Elysia()
     .post("/driver", async ({ body, set }) => {
+        // Create driver
         const newDriver = new Driver({
             name: body.name,
             id: body.id,
@@ -46,6 +56,7 @@ new Elysia()
         if (work_hours) filter.work_hours = work_hours;
 
         try {
+            // checks if the driver exists
             const drivers = await Driver.find(filter);
             if (drivers.length === 0) {
                 set.status = 404;
@@ -59,6 +70,7 @@ new Elysia()
     })
 
     .post("/picker", async ({ body, set }) => {
+        // Create picker
         const newPicker = new Picker({
             name: body.name,
             id: body.id,
@@ -76,7 +88,33 @@ new Elysia()
         return newPicker;
     })
 
+    .get("/picker", async ({ query, set }) => {
+        const { name, id, warehouse, stock_row, work_days, work_hours } = query;
+
+        const filter: Record<string, any> = {};
+        if (name) filter.name = name;
+        if (id) filter.id = id;
+        if (warehouse) filter.warehouse = warehouse;
+        if (stock_row) filter.stock_row = stock_row;
+        if (work_days) filter.work_days = work_days;
+        if (work_hours) filter.work_hours = work_hours;
+
+        try {
+            // checks if the picker exists
+            const pickers = await Picker.find(filter);
+            if (pickers.length === 0) {
+                set.status = 404;
+                return { message: "No pickers found matching the query" };
+            }
+            return pickers;
+        } catch (error) {
+            set.status = 500;
+            return error;
+        }
+    })
+
     .post("/product", async ({ body, set }) => {
+        // Create product
         const newProduct = new Product({
             name: body.name,
             product_id: body.product_id,
@@ -84,7 +122,6 @@ new Elysia()
             stock_row: body.stock_row,
             price: body.price,
             weight: body.weight,
-            priority: body.priority,
             quantity: body.quantity
         });
         try {
@@ -103,6 +140,7 @@ new Elysia()
         if (name) filter.name = name;
 
         try {
+            // Checks if the product exists 
             const products = await Product.find(filter)
             if (products.length === 0) {
                 set.status = 404;
@@ -112,6 +150,7 @@ new Elysia()
                 // Logging the product object for debugging 
                 console.log("Processing product:", product);
 
+                // Checks the quantity of the product
                 if (product.quantity === 0) {
                     return { ...product.toObject(), message: "Out of stock." };
                 }
@@ -121,7 +160,7 @@ new Elysia()
 
         } catch (error) {
             set.status = 500;
-            return error;
+            return { message: "Error fetching product." };
         }
     })
 
@@ -129,6 +168,7 @@ new Elysia()
         const { order_id, products, picker_id } = body as OrderRequestBody;
 
         try {
+            // Checks if the product that wants to be ordere exists
             for (const item of products) {
                 const product = await Product.findById(item.product_id);
                 if (!product) {
@@ -140,12 +180,14 @@ new Elysia()
                     return { message: `Product ${product.name} out of stock.` };
                 }
             }
+            // Reduces the products total quantity
             for (const item of products) {
                 await Product.findByIdAndUpdate(item.product_id, {
                     $inc: { quantity: -item.quantity }
                 });
             }
 
+            // Create order
             const newOrder = new Order({
                 order_id,
                 product: products,
@@ -157,7 +199,60 @@ new Elysia()
             return newOrder;
         } catch (error) {
             set.status = 500
-            return { message: "Error creating order" };
+            return { message: "Error creating order." };
+        }
+    })
+
+    .put("/order/:id", async ({ params, body, set }) => {
+        const { id } = params;
+        const { picker_id, status, products } = body as OrderUpdateBody;
+
+        try {
+            // Checks if the picker exists 
+            if (picker_id) {
+                const picker = await Picker.findById(picker_id);
+                if (!picker) {
+                    set.status = 404;
+                    return { message: "Picker not found." };
+                }
+            }
+
+            // Updates the order 
+            const updatedOrder = await Order.findByIdAndUpdate(
+                id,
+                { picker_id, status, products },
+                { new: true, overwrite: false }
+            );
+
+            if (!updatedOrder) {
+                set.status = 404;
+                return { message: "Order not found." };
+            }
+
+            return updatedOrder;
+        } catch (error) {
+            set.status = 500;
+            return { message: "Error updating order." }
+        }
+    })
+
+    .get("/order", async ({ query, set }) => {
+        const { status } = query;
+
+        const filter: Record<string, any> = {};
+        if (status) filter.status = status;
+
+        try {
+            // Checks if the order exists 
+            const orders = await Order.find(filter);
+            if (orders.length === 0) {
+                set.status = 404;
+                return { message: "Order or orders not found." };
+            }
+            return orders;
+        } catch (error) {
+            set.status = 500;
+            return { message: "Error fetching order." };
         }
     })
 
